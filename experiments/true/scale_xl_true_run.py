cce9f44e065e46879b3f64ef3ba5e33865029756
RyukiKuwahara <ryukikuwahara@outlook.jp>
2024-06-01 17:19:16 +0000

    shift code

--- experiments/true/scale_xl_true_run.py
+++ experiments/true/scale_xl_true_run.py
@@ -36,5 +36,5 @@ for k in tqdm(data.keys()):
     label = data[k]["label"].tolist()
     res = scorer.score(convo, inf_summ)
     results[k] = [res, label]
-    with open(f"results/scale_{size}_true_results.json", "a") as file:
+    with open(f"results/scale_{size}_true_results.json", "w") as file:
         json.dump(results, file)
