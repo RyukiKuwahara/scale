d813f5fbef6fbbe639fc2776fac1127504f229a0
RyukiKuwahara <ryukikuwahara@outlook.jp>
2024-06-02 06:12:23 +0000

    add result xl on true bench

--- experiments/true/analize.py
+++ experiments/true/analize.py
@@ -1,28 +1,25 @@
 from scale_score.eval import evaluate_scale
 from sklearn.metrics import roc_auc_score
+from scipy.stats import pearsonr
 import json
 
-def chank(array: list, w: int = 10000):
-    chunks = [array[i:i+w] for i in range(0, len(array), w)]
-    return chunks
 
-with open("./experiments/true/results/scale_large_true_results.json", "r") as f:
+
+with open("./experiments/true/results/scale_xl_true_results.json", "r") as f:
     data = json.load(f)
 
 
 print(data.keys())
-res, label = data["scores"], data["labels"]
+res, label = data["begin"][0], data["begin"][1]
 print(len(res), len(label))
 
-ress = [chank(res)[0]]
-labels = [chank(label)[0]]
-print(len(ress), len(labels))
+metrics = evaluate_scale(res, label)
+print(metrics)
 
-for res, label in zip(ress, labels):
-    metrics = evaluate_scale(res, label)
-    print(metrics)
+roc_auc = roc_auc_score(label, res)
+print("ROC-AUC:", roc_auc)
 
-    roc_auc = roc_auc_score(label, res)
-    print("ROC-AUC:", roc_auc)
+p_stat = pearsonr(res, label)[0]
+print("p_stat", p_stat)
 
 
